ee5d14ab658df90f467de55cfe6c1a282b501ec6
Nicholas Wong <nickwph@gmail.com>
2020-03-23 10:52:10 +0800

    Added code to get data from fanza.

--- Contents/Code/api/fanza/__init__.py
+++ Contents/Code/api/fanza/__init__.py
@@ -1,30 +1,63 @@
 import requests
+from requests import Response
+from pyquery import PyQuery as pq
+
+api_id = "Ngdp9rsHvCZ9EWrv1LNU"
+affiliate_id = "chokomomo-990"
 
 
-def normalise(keyword):
-    """
-    :type keyword: str
-    :return str: str
-    """
-    keyword = keyword.lower()
-    keyword = keyword.strip()
-    keyword = keyword.replace("-", "00")
-    keyword = keyword.replace("dsvr", "13dsvr")
-    keyword = keyword.replace("313dsvr", "13dsvr")
-    keyword = keyword.replace("avopvr", "h_1158avopvr")
-    keyword = keyword.replace("kmvr", "84kmvr")
-    keyword = keyword.replace("bi84kmvr", "h_1285bikmvr")
-    keyword = keyword.replace("bzvr", "84bzvr")
-    keyword = keyword.replace("crvr", "h_1155crvr")
-    keyword = keyword.replace("exvr", "84exvr")
-    keyword = keyword.replace("vvvr", "84vvvr")
-    keyword = keyword.replace("dtvr", "24dtvr")
-    keyword = keyword.replace("scvr", "h_565scvr")
-    keyword = keyword.replace("wpvr", "2wpvr")
-    keyword = keyword.replace("mxvr", "h_1282mxvr")
-    keyword = keyword.replace("tmavr", "55tmavr")
-    keyword = keyword.replace("vovs", "h_1127vovs")
-    keyword = keyword.replace("cafr", "h_1116cafr")
-    keyword = keyword.replace("tpvr", "h_1256tpvr")
-    print keyword
-    return keyword
+class FanzaApi(object):
+
+    @staticmethod
+    def normalize(keyword):
+        """
+        :type keyword: str
+        :rtype: str
+        """
+        keyword = keyword.lower()
+        keyword = keyword.strip()
+        keyword = keyword.replace("-", "00")
+        keyword = keyword.replace("dsvr", "13dsvr")
+        keyword = keyword.replace("313dsvr", "13dsvr")
+        keyword = keyword.replace("avopvr", "h_1158avopvr")
+        keyword = keyword.replace("kmvr", "84kmvr")
+        keyword = keyword.replace("bi84kmvr", "h_1285bikmvr")
+        keyword = keyword.replace("bzvr", "84bzvr")
+        keyword = keyword.replace("crvr", "h_1155crvr")
+        keyword = keyword.replace("exvr", "84exvr")
+        keyword = keyword.replace("vvvr", "84vvvr")
+        keyword = keyword.replace("dtvr", "24dtvr")
+        keyword = keyword.replace("scvr", "h_565scvr")
+        keyword = keyword.replace("wpvr", "2wpvr")
+        keyword = keyword.replace("mxvr", "h_1282mxvr")
+        keyword = keyword.replace("tmavr", "55tmavr")
+        keyword = keyword.replace("vovs", "h_1127vovs")
+        keyword = keyword.replace("cafr", "h_1116cafr")
+        keyword = keyword.replace("tpvr", "h_1256tpvr")
+        return keyword
+
+    @staticmethod
+    def get_item_list(keyword):
+        """
+        :type keyword: str
+        :rtype: Response
+        """
+        return requests.get("https://api.dmm.com/affiliate/v3/ItemList", params={
+            "api_id": api_id,
+            "affiliate_id": affiliate_id,
+            "site": "FANZA",
+            "service": "digital",
+            "floor": "videoa",
+            "hits": "10",
+            "sort": "date",
+            "keyword": FanzaApi.normalize(keyword),
+            "output": "json"
+        })
+
+    @staticmethod
+    def get_product_description(url):
+        """
+        :type url: str
+        :rtype: str
+        """
+        return pq(url)(".mg-b20.lh4").text().rstrip()
--- Contents/Code/api/fanza/test___init__.py
+++ Contents/Code/api/fanza/test___init__.py
@@ -1,9 +1,22 @@
 from unittest import TestCase
 
-from api.fanza import normalise
+from munch import Munch
+
+from api.fanza import FanzaApi
 
 
-class Test(TestCase):
-    def test_normalise(self):
-        normalise("ASDASD")
-        # self.fail()
+class TestFanzaApi(TestCase):
+
+    def test_normalize(self):
+        self.assertEqual(FanzaApi.normalize("SSNI-558"), "ssni00558")
+
+    def test_get_item_list(self):
+        response = FanzaApi.get_item_list("SSNI-558")
+        body = Munch.fromDict(response.json())
+        self.assertEqual(body.result.status, 200)
+        self.assertEqual(body.result.total_count, 1)
+        self.assertEqual(body.result['items'][0].content_id, "ssni00558")  # items is a reserved variable
+
+    def test_get_product_description(self):
+        description = FanzaApi.get_product_description("https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=ssni00558/")
+        self.assertEqual(len(description), 193)  # too hard to check the whole string
