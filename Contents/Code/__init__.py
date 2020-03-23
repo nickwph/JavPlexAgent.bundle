7802f1f2ad4402db28483352e411758b393e874d
Nicholas Wong <nickwph@gmail.com>
2020-03-23 22:53:16 +0800

    Updated.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,11 @@
 # from environments import is_local_debugging
 # from framework import Log
+import os
+import sys
+
+print os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
 # noinspection PyPep8Naming
