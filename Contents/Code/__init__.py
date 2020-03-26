f0e7b312fd3406d9820d3de725f396f7470331fd
Nicholas Wong <nickwph@gmail.com>
2020-03-26 23:03:28 +0800

    Updated.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,6 +3,7 @@ from environments import is_local_debugging
 if is_local_debugging:
     from framework.framework_log import Log
 Log.Error("=========== Start ==========")
+Log.Error("sys.executable: {}".format(sys.executable))
 for i, path in enumerate(sys.path):
     Log.Error("sys.path[{}]: {}".format(i, path))
 
