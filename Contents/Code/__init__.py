ac8ea639ff2e4ac81045324aaaff662457532071
Nicholas Wong <nickwph@gmail.com>
2020-03-26 23:00:01 +0800

    Updated.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,5 @@
 import sys
+from environments import is_local_debugging
 if is_local_debugging:
     from framework.framework_log import Log
 Log.Error("=========== Start ==========")
@@ -6,7 +7,6 @@ for i, path in enumerate(sys.path):
     Log.Error("sys.path[{}]: {}".format(i, path))
 
 from agent_movie_jav import JavMovieAgent
-from environments import is_local_debugging
 
 
 
