a1c145982ae0d38f0c3faf0114a38a1415cb7b50
Nicholas Wong <nickwph@gmail.com>
2020-09-10 01:01:49 +0800

    Refined bongo converter and added hyphen to bongo.

--- Contents/Code/fanza_helper.py
+++ Contents/Code/fanza_helper.py
@@ -6,11 +6,7 @@ def convert_product_id_to_bongo(product_id):
     :type product_id: str
     """
     bongo = product_id.upper()
-    # remove h_ prefix
-    if bongo.startswith('H_'):
-        bongo = bongo[len('H_'):]
-    # normalize bongo
-    match = re.match("(\d*)([a-zA-Z]+)(00)?(\d+)", bongo, re.IGNORECASE)  # noqa: W605
+    match = re.match("(H_)?(\d*)([a-zA-Z]+)(00)?(\d+)", bongo, re.IGNORECASE)  # noqa: W605
     if match:
-        bongo = match.group(2) + match.group(4)
+        bongo = match.group(3) + "-" + match.group(5)
     return bongo
