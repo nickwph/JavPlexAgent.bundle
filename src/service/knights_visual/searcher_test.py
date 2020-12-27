c4a08a4b06403429e1ff0eb892fbda4279912ed9
Nicholas Wong <nickwph@gmail.com>
2020-12-28 04:48:12 +0800

    Fixed unit test.

--- src/service/knights_visual/searcher_test.py
+++ src/service/knights_visual/searcher_test.py
@@ -15,7 +15,7 @@ class Test(TestCase):
         self.assertEqual(u"ja", results[0].lang)
         self.assertEqual(u"https://www.knights-visual.com/wp-content/uploads/2014/11/kv-094-115x85.jpg",
                          results[0].thumb)
-        self.assertEqual(66, results[0].score)
+        self.assertEqual(100, results[0].score)
         self.assertEqual(2014, results[0].year)
         self.assertEqual(u"knights-visual-kv-094", results[0].id)
         self.assertEqual(u"kv-094 おしゃぶり予備校32 みづなれい", results[0].name)
