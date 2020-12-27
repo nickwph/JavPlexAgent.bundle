17431ed489fb2110319074d83414402fec8589d7
Nicholas Wong <nickwph@gmail.com>
2020-12-28 03:31:25 +0800

    Print with nicer coloring.

--- build_log.py
+++ build_log.py
@@ -24,26 +24,26 @@ def tail_log():
     if platform.system().lower() == 'darwin':
         cprint("> tailing log")
         log_path = "~/Library/Logs/Plex Media Server/PMS Plugin Logs/com.nicholasworkshop.javplexagent.log".replace(" ", "\ ")
-        color_debug = '/DEBUG/ {print "\033[35m" $0 "\033[39m"; next}'
-        color_info = '/INFO/ {print "\033[39m" $0 "\033[39m"; next}'
-        color_warn = '/WARN/ {print "\033[33m" $0 "\033[39m"; next}'
-        color_error = '/ERROR/ {print "\033[31m" $0 "\033[39m"; next}'
-        color_critical = '/CRITICAL/ {print "\033[41m\033[37m" $0 "\033[39m\033[49m"; next}'
-        color_exception = '/EXCEPTION/ {print "\033[41m\033[31m" $0 "\033[39m\033[49m"; next}'
-        color_default = '{print $0}'
+        color_debug = '/DEBUG/ {print "\033[35m" $0; next}'
+        color_info = '/INFO/ {print "\033[39m" $0; next}'
+        color_warn = '/WARN/ {print "\033[33m" $0; next}'
+        color_error = '/ERROR/ {print "\033[31m" $0"; next}'
+        color_critical = '/CRITICAL/ {print "\033[41m\033[37m" $0; next}'
+        color_exception = '/EXCEPTION/ {print "\033[41m\033[31m" $0; next}'
+        color_default = '{print $0 "\033[39m\033[49m"}'
         os.system("tail -F -n 200 {} | awk '{} {} {} {} {} {} {}'".format(log_path, color_debug, color_info, color_warn, color_error, color_critical, color_exception, color_default))
 
     # ubuntu
     elif platform.system().lower() == 'linux':
         cprint("> tailing log")
         log_path = "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/PMS Plugin Logs/com.nicholasworkshop.javplexagent.log".replace(" ", "\ ")
-        color_debug = '/DEBUG/ {print "\033[35m" $0 "\033[39m"; next}'
-        color_info = '/INFO/ {print "\033[39m" $0 "\033[39m"; next}'
-        color_warn = '/WARN/ {print "\033[33m" $0 "\033[39m"; next}'
-        color_error = '/ERROR/ {print "\033[31m" $0 "\033[39m"; next}'
-        color_critical = '/CRITICAL/ {print "\033[41m\033[37m" $0 "\033[39m\033[49m"; next}'
-        color_exception = '/EXCEPTION/ {print "\033[41m\033[31m" $0 "\033[39m\033[49m"; next}'
-        color_default = '{print $0}'
+        color_debug = '/DEBUG/ {print "\033[35m" $0; next}'
+        color_info = '/INFO/ {print "\033[39m" $0; next}'
+        color_warn = '/WARN/ {print "\033[33m" $0; next}'
+        color_error = '/ERROR/ {print "\033[31m" $0"; next}'
+        color_critical = '/CRITICAL/ {print "\033[41m\033[37m" $0; next}'
+        color_exception = '/EXCEPTION/ {print "\033[41m\033[31m" $0; next}'
+        color_default = '{print $0 "\033[39m\033[49m"}'
         os.system("tail -F -n 200 {} | awk '{} {} {} {} {} {} {}'".format(log_path, color_debug, color_info, color_warn, color_error, color_critical, color_exception, color_default))
 
     # windows
