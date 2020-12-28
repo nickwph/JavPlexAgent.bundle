98c3c3f7dfcb62da67aef106f0904377e69dc062
Nicholas Wong <nickwph@gmail.com>
2020-12-28 14:19:55 +0800

    Added log tailing for windows.

--- build_log.py
+++ build_log.py
@@ -1,20 +1,9 @@
-import argparse
-import datetime
 import os
 import platform
-import re
-import sys
-import tarfile
-from os.path import expanduser
-from shutil import rmtree, copyfile, copytree
 
 import colorama
-import psutil
 from colorama import Fore, Style
 from termcolor import cprint
-
-from build_patch import patch_image_file, patch_windows_pillow
-from build_replacement import extract_replacements_from_filenames
 
 # allow command line coloring
 colorama.init()
@@ -39,8 +28,9 @@ def tail_log():
         os.system("tail -F -n 200 {} | awk '{}'".format(log_path, colors))
     elif platform.system().lower() == 'windows': # windows
         cprint("> tailing log")
-        cprint("sorry it is not available yet, open this file by yourself", 'yellow')
-        cprint("%LOCALAPPDATA%\Plex Media Server\Logs\PMS Plugin Logs\com.nicholasworkshop.javplexagent", 'yellow')
+        cprint("sorry coloring is not available yet", 'yellow')
+        log_path = "%LOCALAPPDATA%\Plex Media Server\Logs\PMS Plugin Logs\com.nicholasworkshop.javplexagent.log"
+        os.system('etc\UnxUpdates\\tail -F -n 200 "{}"'.format(log_path))
 
 
 if __name__ == '__main__':
